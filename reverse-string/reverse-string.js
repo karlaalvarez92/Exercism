3207c544b4be465049fcb4af0cf885f60d212ddf
Karla <karla.alvarez@michelada.io>
2020-02-27 17:40:53 -0600

    join successfully implemented and unnecessary code is removed

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,14 +1,3 @@
 export const reverseString = string => {
-  if (string) {
-    let reverseString = string.split("").reverse().toString();
-
-    let replaceAll = (text, search, replace) => {
-      while (text.toString().indexOf(search) != -1)
-        text = text.replace(search, replace);
-      return text;
-    };
-    return replaceAll(reverseString, ",", "");
-  } else {
-    return "";
-  }
+    return string.split("").reverse().join("");
 };
