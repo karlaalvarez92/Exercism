721c40e7ff1cd5b687762fbcdcf82a28f1bb6905
Karla <karla.alvarez@michelada.io>
2020-02-27 09:47:48 -0600

    Functions are changed to english

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,20 +1,20 @@
 export const rows = n => {
-let piramide= [];
+let pyramid= [];
 
 for(let row=0; row<n; row++){
-  let listas= []
+  let rowArray= []
 
  for(let column=0; column<=row; column++){
    let element = 0;
   if(column===0 || column===row){
     element=1
   }else{
-    element= piramide[row-1][column-1] + piramide[row-1][column];
+    element= pyramid[row-1][column-1] + pyramid[row-1][column];
   }
-   listas.push(element);
+  rowArray.push(element);
  }
- piramide.push(listas);
+ pyramid.push(rowArray);
 }
-return piramide;
+return pyramid;
 }
 
