9a467163619d8f355bc9c88e9629dd3cf0bd7a96
Karla Alvarez <karlaalvarez@Karlas-MacBook-Pro.local>
2020-03-02 23:38:39 -0600

    Perfect-numbers exercise is solved, unit test are passed, I wonder how far it is wise to put everthing in methods

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = () => {
-  throw new Error("Remove this statement and implement this function");
+export const classify = (number) => {
+ const
+ PerfectNumber= 'perfect',
+ AbundantNumber= 'abundant',
+ DeficentNumber= 'deficient',
+ NoNaturalNumber= 'Classification is only possible for natural numbers.',
+ 
+ isntNaturalNumber=() => number <= 0,
+ isPerfectNumber=()=> sumOfFactors == number,
+ isAbundantNumber=()=> sumOfFactors > number,
+ isDeficentNumber=()=> sumOfFactors < number;
+
+ let sumOfFactors=0;
+
+ if(isntNaturalNumber(number))throw new Error(NoNaturalNumber);
+
+ for(let i=0; i<number; i++){
+    if(number % i == 0)
+      sumOfFactors += i
+  };
+
+if(isPerfectNumber(sumOfFactors, number)){return PerfectNumber}
+if(isAbundantNumber(sumOfFactors, number)){return AbundantNumber}
+if(isDeficentNumber(sumOfFactors, number)){return DeficentNumber}
+
 };
+
+/* if(sumOfFactors ==number){return PerfectNumber}
+if(sumOfFactors < number){return DeficentNumber}
+if(sumOfFactors > number){return AbundantNumber} */
