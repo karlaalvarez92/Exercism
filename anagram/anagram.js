1e42720b784cf9f8c3cb472bc83612ba1323b197
Karla Alvarez <karlaalvarez@Karlas-MacBook-Pro.local>
2020-03-03 00:27:01 -0600

    Pending resolve case of arrays with equal words, returned empty arrays

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,8 +1,24 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//agregar para comparar si es la misma retornar array vacío
 
-export const findAnagrams = () => {
-  throw new Error("Remove this statement and implement this function");
+export const findAnagrams = (word, array) => {
+  let
+  sortedWord= word.toUpperCase().split("").sort().join(""),
+  sortedArray= [],
+  indexMatch=[],
+  arrayWithMatches=[];
+
+  for(let i=0; i< array.length; i++){
+    sortedArray.push(array[i].toUpperCase().split("").sort().join("")); 
+  }
+
+  for(let i=0; i< sortedArray.length; i++){
+    if(sortedArray[i] == sortedWord){
+      indexMatch.push(i)
+    }
+  }
+
+  for(let i=0; i<indexMatch.length; i++){
+    arrayWithMatches.push(array[indexMatch[i]])
+  }
+  return arrayWithMatches
 }
