7e255fc114cea419238a63c511b5dfdf739e9b74
Karla Alvarez <karlaalvarez@Karlas-MacBook-Pro.local>
2020-03-02 20:14:07 -0600

    Raindrops exercise is solved, passes unit test. I try to create methods and calling them

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -3,6 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+export const convert = (number) => {
+  const 
+  threeIsFactor = "Pling",
+  fiveIsFactor= "Plang",
+  sevenIsFactor="Plong",
+
+  isDivisibleBy =(number, factor)=> number % factor == 0,
+
+isDivisibleByThree = isDivisibleBy(number, 3),
+  isDivisibleByFive=isDivisibleBy(number, 5) ,
+  isDivisibleBySeven=isDivisibleBy(number, 7);
+  
+  let string= "";
+  if(!isDivisibleBySeven && !isDivisibleByThree && !isDivisibleByFive){
+    return number.toString()
+  }else if(isDivisibleByThree){
+    string+= threeIsFactor
+  } 
+  if(isDivisibleByFive){
+    string += fiveIsFactor
+  }
+  if(isDivisibleBySeven){
+    string +=sevenIsFactor
+  }
+  
+return string
 };
