4b2920547ede660126144846e126af64ee43a5ba
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-04 16:00:31 -0600

    Exercises are added forThursday, March 5

---
+++ javascript/series/series.js
@@ -0,0 +1,24 @@
+import { threadId } from "worker_threads";
+
+export class Series {
+  constructor(string) {
+    this.numbersArray = string.split("").map(element => Number(element));
+  }
+
+  get digits() {
+    return this.numbersArray;
+  }
+
+  slices(n) {
+    if (this.numbersArray.length < n) {
+      throw new Error("Slice size is too big.");
+    }
+
+    let seriesArray = [];
+
+    for (let i = 0; i <= this.numbersArray.length - n; i++) {
+      seriesArray.push(this.numbersArray.slice(i, i + n));
+    }
+    return seriesArray;
+  }
+}
