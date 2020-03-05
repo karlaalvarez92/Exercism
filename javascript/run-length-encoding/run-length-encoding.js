fe5ca2b956c2b93c998159bc2933c83aa3ce11ce
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-05 14:52:40 -0600

    Run-length-encoding exercise is solved, unit test are passed. Awaiting review to clean and efficient code.

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,12 +1,75 @@
-//
-// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//Necesito iterar mi string y comparar las letras,
+//sumar las letras que sean iguales y retornar #Letra,
+//si la letra es diferente cortar el contador y regresar la
+//letra siguiente
 
-export const encode = () => {
-  throw new Error("Remove this statement and implement this function");
+//Pasa los test de encode
+/* export const encode = string => {
+  let count = 1;
+  let encoding = "";
+  let prev = "";
+
+  string.split("").forEach(char => {
+    if (char !== prev) {
+      if (count == 1) {
+        encoding += prev;
+        count = 1;
+        prev = char;
+      } else {
+        encoding += count + prev;
+        count = 1;
+        prev = char;
+      }
+    } else {
+      count++;
+    }
+  });
+  encoding += count + prev;
+  console.log(string, encoding); */
+
+export const encode = string => {
+  let prev = string[0],
+    count = 1,
+    encoding = "";
+
+  for (let i = 1; i <= string.length; i++) {
+    if (string[i] !== prev) {
+      if (count == 1) {
+        encoding += prev;
+        count = 1;
+        prev = string[i];
+      } else {
+        encoding += count + prev;
+        count = 1;
+        prev = string[i];
+      }
+    } else {
+      count++;
+    }
+  }
+  encoding += count + prev;
+  //Cuando se detenía en el último carácter me escribía el 1 antes de la letra.
+  encoding = encoding.replace("NaN", "");
+  return encoding;
 };
 
-export const decode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decode = encoded => {
+  let count = "";
+  let decoding = "";
+
+  encoded.split("").forEach(char => {
+    if (char.match(/\d/)) {
+      count += char;
+      return;
+    }
+
+    count = count ? parseInt(count, 10) : 1;
+
+    for (let i = 0; i < count; i += 1) {
+      decoding += char;
+    }
+    count = "";
+  });
+
+  return decoding;
 };
