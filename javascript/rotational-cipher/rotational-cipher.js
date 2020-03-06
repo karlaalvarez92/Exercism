e1d1c8de1f5fc221be8177e0abc26c58ab5a6cbe
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-06 10:47:08 -0600

    A solution implemented by Silvana is added. Check if you can do descriptive methods and try to solve it with a map

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,10 +1,21 @@
-//
-// This is only a SKELETON file for the 'Rotational Cipher' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/*Revisar como poner los if dentro de métodos y hacer lo mismo con un map
+  Èxito!!! */
 
 export class RotationalCipher {
-  static rotate() {
-    throw new Error("Remove this statement and implement this function");
+  static rotate(string, rotate) {
+    let cipherString = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let characterCode = string[i].charCodeAt(0);
+      if (characterCode >= 65 && characterCode <= 90) {
+        characterCode += rotate;
+        if (characterCode > 90) characterCode -= 26;
+      } else if (characterCode >= 97 && characterCode <= 122) {
+        characterCode += rotate;
+        if (characterCode > 122) characterCode -= 26;
+      }
+      cipherString += String.fromCharCode(characterCode);
+    }
+    return cipherString;
   }
 }
