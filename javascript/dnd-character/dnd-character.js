f2fddda9dae77a2e76b12f2df1cd10e301903fbc
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-06 01:10:14 -0600

    Dnd-character exercise is solved, unit test are passed. I need to clean and sort code, and remove unnecesary comments

--- javascript/dnd-character/dnd-character.js
+++ javascript/dnd-character/dnd-character.js
@@ -1,42 +1,73 @@
-//
-// This is only a SKELETON file for the 'DnD Character' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const abilityModifier = () => {
-  throw new Error("Remove this statement and implement this function");
+//Encuentra el modificador de constitución de tu personaje restando 10 de
+//la constitución de tu personaje, divide entre 2 y redondea hacia abajo.
+export const abilityModifier = modifier => {
+  if (modifier < 3) {
+    throw new Error("Ability scores must be at least 3");
+  }
+  if (modifier > 18) {
+    throw new Error("Ability scores can be at most 18");
+  }
+  return Math.floor((modifier - 10) / 2);
 };
 
+//Escriba un generador de caracteres aleatorios que siga las reglas anteriores.
 export class Character {
+  constructor() {
+    this._constitution = Character.rollAbility();
+    this._strength = Character.rollAbility();
+  }
   static rollAbility() {
-    throw new Error("Remove this statement and implement this function");
+    //Genera un número random del 1-6 simula tirar un dado
+    let randomCharacter = () => Math.floor(Math.random() * 6 + 1);
+    //Array dónde se guardan los tiros de dado
+    let _rollAbility = Array(4);
+    _rollAbility.fill("").map(e => _rollAbility.push(randomCharacter()));
+
+    let abilityValue = _rollAbility
+      .sort((a, b) => b - a)
+      .slice(0, 3)
+      .reduce((a, b) => (a += b));
+    //console.log(keac);
+    return abilityValue;
+    //console.log(randomCharacter);
+    //retorna habilidad
+    //return ability;
   }
 
   get strength() {
-    throw new Error("Remove this statement and implement this function");
+    return this._strength;
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get dexterity() {
-    throw new Error("Remove this statement and implement this function");
+    return Character.rollAbility();
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get constitution() {
-    throw new Error("Remove this statement and implement this function");
+    return this._constitution;
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get intelligence() {
-    throw new Error("Remove this statement and implement this function");
+    return Character.rollAbility();
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get wisdom() {
-    throw new Error("Remove this statement and implement this function");
+    return Character.rollAbility();
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get charisma() {
-    throw new Error("Remove this statement and implement this function");
+    return Character.rollAbility();
+    //throw new Error("Remove this statement and implement this function");
   }
 
   get hitpoints() {
-    throw new Error("Remove this statement and implement this function");
+    let addition = 10;
+
+    return addition + abilityModifier(this._constitution);
+    // throw new Error("Remove this statement and implement this function");
   }
 }
