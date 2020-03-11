69144a2dfa0f7ea92a56578e7f43429dbd8af6aa
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-11 12:06:38 -0600

    Iterated Leap exercise, dead code is removed

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,13 +1,3 @@
 export const isLeap = year => {
-  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? true : false;
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 };
-
-
-
-// With a ternary function code is reduced
-
-// if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-//   return true
-// }else{
-//   return false
-// }
