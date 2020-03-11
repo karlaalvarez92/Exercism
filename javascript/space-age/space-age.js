8dc4503e0a2b99e0b1820d5d634cb399e1dc0fea
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-11 11:11:03 -0600

    Iterated Space-age  exercise, dead code is removed

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,27 +1,18 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const age = (planet, ageInSeconds) => {
- return (ageInSeconds/(YEAR_IN_SECONDS*(PLANETS[planet])).toFixed(2))
+  return Number(
+    (ageInSeconds / (YEAR_IN_SECONDS * PLANETS[planet])).toFixed(2)
+  );
 };
 
-const YEAR_IN_SECONDS= 31557600
+const YEAR_IN_SECONDS = 31557600;
 
 const PLANETS = {
-mercury: 0.2408467,
-venus: 0.61519726,
-earth: 1,
-mars: 1.8808158,
-jupiter: 11.862615,
-saturn: 29.447498,
-uranus: 84.016846,
-neptune: 164.79132 
-}
-
-// return Number(
-//   convertToSeconds(planet, seconds).toFixed(2)
-// );
-
-// const convertToSeconds= (planet, seconds)
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+};
