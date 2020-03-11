97efc99a5b6377e3f66de7b3fa81d8b0aa3101a0
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-11 16:51:57 -0600

    Secret-handshake exercise solved, pass unit test. Awaiting review for iteration.

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,8 +1,34 @@
 //
 // This is only a SKELETON file for the 'Secret Handshake' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
 
-export const commands = () => {
-  throw new Error("Remove this statement and implement this function");
+export const commands = number => {
+  let binary = number.toString(2);
+  let array = [];
+  let actions = [];
+  let binaryArray = binary.split("").reverse();
+
+  //Intenté hacerlo con map pero el elemento era "el mismo" y me retornaba un
+  //indexOf incorrecto
+  for (let i = 0; i < binaryArray.length; i++) {
+    if (binaryArray[i] == 1) {
+      array.push(i);
+    }
+  }
+
+  array.map(el => {
+    if (el == 4) {
+      actions.reverse();
+    } else {
+      actions.push(ACTIONS[el]);
+    }
+  });
+  return actions;
+};
+
+const ACTIONS = {
+  0: "wink",
+  1: "double blink",
+  2: "close your eyes",
+  3: "jump"
 };
