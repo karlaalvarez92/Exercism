9e9f541468abec54a203f6a79af6c968cdc3a62f
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-11 10:48:50 -0600

    Iterated Rna-transcription exercise. First iteration is preserved as an example

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,32 +1,34 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// export const toRna = rna => {
-//   let str = "";
-
-//   for (let i = 0; i < rna.length; i++) {
-//     if (rna[i] === "G") {
-//       str += rna[i].replace("G", "C");
-//     } else if (rna[i] === "C") {
-//       str += rna[i].replace("C", "G");
-//     } else if (rna[i] === "T") {
-//       str += rna[i].replace("T", "A");
-//     } else {
-//       str += rna[i].replace("A", "U");
-//     }
-//   }
-//   return str;
-// };
-
+/*Por cada elemento de la cadena el map retorna el valor coincidente 
+y lo une en un string. */
 export const toRna = rna => {
-  return rna.split('').map((value) => NUCLEOTIDES[value]).join('')
+  return rna
+    .split("")
+    .map(value => NUCLEOTIDES[value])
+    .join("");
 };
 
 const NUCLEOTIDES = {
-  "G": "C",
-  "C": "G",
-  "T": "A",
-  "A": "U",
-}
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
+};
+
+// Primera solución encontrada, pasa todos los test
+
+/* export const toRna = rna => {
+  let str = "";
+
+  for (let i = 0; i < rna.length; i++) {
+    if (rna[i] === "G") {
+      str += rna[i].replace("G", "C");
+    } else if (rna[i] === "C") {
+      str += rna[i].replace("C", "G");
+    } else if (rna[i] === "T") {
+      str += rna[i].replace("T", "A");
+    } else {
+      str += rna[i].replace("A", "U");
+    }
+  }
+  return str;
+}; */
