02d8582f502ac9c970edd6ab9fe19e523c852a60
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-12 09:57:51 -0600

    Progress is saved, 3 unit test are missing

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,8 +1,51 @@
-//
-// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const answer = question => {
+  let clean = question
+    .replace(/(What\sis\s?)/g, "")
+    .replace("?", "")
+    .replace(/(by)/g, "")
+    .replace(/(plus)/g, "+")
+    .replace(/(minus)/g, "-")
+    .replace(/(multiplied)/g, "*")
+    .replace(/(divided)/g, "/")
+    .replace(/\s+/g, " ");
 
-export const answer = () => {
-  throw new Error("Remove this statement and implement this function");
+  console.log(question, [clean]);
+
+  let array = question.split(" ");
+  if (array[0] !== "What" || clean.match(/[A-Za-z]/g)) {
+    throw new Error("Unknown operation");
+  }
+
+  if (!clean.match(/[A-Za-z\+\-\*\/]/g)) {
+    return Number(clean);
+  }
+
+  if (
+    question.match(/(\d\s\d)/g) ||
+    !question.match(
+      /-?\d+/g
+    ) /*||
+    //!clean.match(/(-?\d+\s)[\+\-\*\/]\s(-?\d+)/g)
+
+    //Al parecer este pedacito no es necesario||
+    clean == " "*/
+  ) {
+    throw new Error("Syntax error");
+  }
+
+  let firstOperation = eval(
+    clean
+      .split(" ")
+      .slice(0, 3)
+      .join(" ")
+  );
+
+  let secondOperation = clean
+    .split(" ")
+    .slice(3, 5)
+    .join(" ");
+
+  console.log(question, clean);
+
+  return eval(firstOperation + secondOperation);
 };
