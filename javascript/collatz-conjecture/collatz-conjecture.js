1ff2b968cac1d2ef87311e3d37d3cd4e208ca6f4
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-13 11:32:10 -0600

    Added comment that clarifies the exercise

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,17 +1,25 @@
-export const steps = (number) => {
+/* Tome cualquier número entero positivo n. Si n es par, divida n entre 2 
+para obtener n / 2. Si n es impar, multiplique n por 3 y sume 1 
+para obtener 3n + 1. Repita el proceso indefinidamente. 
+La conjetura establece que no importa con qué número comiences, 
+siempre llegarás a 1 eventualmente.
+
+Dado un número n, devuelve el número de pasos necesarios para llegar a 1. */
+
+export const steps = number => {
   if (number <= 0) {
-    throw Error('Only positive numbers are allowed')
+    throw Error("Only positive numbers are allowed");
   }
 
-  let steps=0;
+  let steps = 0;
 
-  while(number !==1){
-    if(number % 2 == 0){
-      number = number/2;
-    }else{
-      number = 3*number+1
+  while (number !== 1) {
+    if (number % 2 == 0) {
+      number = number / 2;
+    } else {
+      number = 3 * number + 1;
     }
-    steps++
+    steps++;
   }
-  return steps
+  return steps;
 };
