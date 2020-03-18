811bed340dc4b8022461abad5331ce7b9381ee82
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-17 18:05:02 -0600

    Nucleotide-count exercise is solved, unit test are passed

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,10 +1,16 @@
-//
-// This is only a SKELETON file for the 'Nucleotide-Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class NucleotideCounts {
-  static parse() {
-    throw new Error("Remove this statement and implement this function");
+  static parse(string) {
+    let array = string.split("");
+    const NUCLEOTIDE = ["A", "C", "G", "T"];
+    let counter = [0, 0, 0, 0];
+
+    array.map(el => {
+      if (!NUCLEOTIDE.includes(el)) {
+        throw new Error("Invalid nucleotide in strand");
+      } else {
+        counter[NUCLEOTIDE.indexOf(el)] += 1;
+      }
+    });
+    return counter.join(" ");
   }
 }
