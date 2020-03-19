11bc98657fdd9210e1e09e6b9fefd3d2672fac29
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-18 22:17:07 -0600

    Luhn exercise is solved with for loop and reduce, pass unit test. Awaiting feedback for new iteration

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -1,8 +1,21 @@
-//
-// This is only a SKELETON file for the 'Luhn' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const valid = number => {
+  let cleanNumber = number.replace(/\s/g, "");
+  if (number <= 1 || number.match(/[A-Za-z:#$-]/g)) {
+    return false;
+  }
 
-export const valid = () => {
-  throw new Error("Remove this statement and implement this function");
+  let array = cleanNumber.split("");
+  let luhnNumber = array.map(el => parseInt(el));
+
+  for (let i = luhnNumber.length - 2; i >= 0; i -= 2) {
+    if (luhnNumber[i] * 2 > 9) {
+      luhnNumber[i] = luhnNumber[i] * 2 - 9;
+    } else {
+      luhnNumber[i] = luhnNumber[i] * 2;
+    }
+  }
+
+  let validate = luhnNumber.reduce((a, b) => a + b, 0);
+
+  return validate % 10 == 0;
 };
