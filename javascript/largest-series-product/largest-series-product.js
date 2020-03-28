31427e7a4fd6385fd4af84e5ed47d11c1017b74c
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-27 21:42:52 -0600

    Solve exercise, pass unit test. Wait revision for new iteration

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,8 +1,32 @@
-//
-// This is only a SKELETON file for the 'Largest Series Product' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const largestProduct = (chain, size) => {
+  let array = chain.split("").map(el => Number(el));
+  let series = [];
 
-export const largestProduct = () => {
-  throw new Error("Remove this statement and implement this function");
+  if (chain.match(/[a-zA-Z]\w+/g)) {
+    throw new Error("Digits input must only contain digits");
+  }
+  if (size == 0) {
+    return 1;
+  }
+  if (size < 0) {
+    throw new Error("Span must be greater than zero");
+  }
+  if (chain.length < size) {
+    throw new Error("Span must be smaller than string length");
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    let slice = array.slice(i, i + size);
+    if (slice.length == size) {
+      series.push(slice);
+    }
+  }
+  const reducer = (accumulator, currentValue) => accumulator * currentValue;
+
+  let products = series.map(el => el.reduce(reducer));
+  products.sort((a, b) => {
+    return a - b;
+  });
+
+  return products[products.length - 1];
 };
