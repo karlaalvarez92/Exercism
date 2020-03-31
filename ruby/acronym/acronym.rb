40c3c22388c9ecd5e4b4caa8a295f425d17f6390
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-31 14:59:26 -0600

    Iterate exercise with classmates solutions

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,13 +1,13 @@
 class Acronym
     def self.abbreviate(string)
-     array = string.upcase.gsub(/[^A-Z0-9']+/, " ").split(" ")
-     
-     acronym = ""
-     array.each_with_index do |el, index| 
-        acronym += array[index][0]
-     end
-     acronym
+        string.split(/ |-/).map{|word| word[0]}.join("").upcase
     end
 end
+
+
+
+
+
+
 
 
