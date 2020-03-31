69dda6095503491df7003fdb31e0487995f82ad8
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-31 00:42:28 -0600

    Solve exercise, pass unit test

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,35 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
+class Raindrops
+    def self.convert(number)
+        threeIsFactor = "Pling"
+        fiveIsFactor = "Plang"
+        sevenIsFactor = "Plong"
 
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
+        def self.isDivisibleBy(number, factor)
+          number % factor == 0
+        end
+        
+        soundOfRain = ""
+
+        if !isDivisibleBy(number, 3) &&
+           !isDivisibleBy(number, 5) &&
+           !isDivisibleBy(number, 7)
+          
+           soundOfRain += number.to_s;
+        end
+       
+        if isDivisibleBy(number, 3)
+           soundOfRain += threeIsFactor;
+        end
+          
+        if isDivisibleBy(number, 5) 
+           soundOfRain += fiveIsFactor;
+        end
+          
+        if isDivisibleBy(number, 7) 
+           soundOfRain += sevenIsFactor;
+        end
+         
+        soundOfRain; 
+    end
+end
 
