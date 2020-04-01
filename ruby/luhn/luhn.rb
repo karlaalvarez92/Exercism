5bf709c7efd7ac68399e7f7b83fa67d4a22a8e8d
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-01 11:54:28 -0600

    Update solution

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,13 +1,13 @@
 class Luhn
     def self.valid?(number)
         cleanNumber= number.gsub(/\s/, "")
-        if cleanNumber.length <=1 || cleanNumber.match(/[A-Za-z:#$-]/)
+        if cleanNumber.length <=1 || cleanNumber.match?(/[A-Za-z:#$-]/)
             return false
         else
             luhnNumber = cleanNumber.split("").reverse.map &:to_i
             i=1
 
-            while i <= luhnNumber.length do
+            while i <= luhnNumber.length-1 do
                 if (luhnNumber[i] * 2 > 9) 
                    luhnNumber[i] = luhnNumber[i] * 2 - 9
                    i+=2
@@ -16,12 +16,11 @@ class Luhn
                    i+=2
                 end 
                 
-                validate = luhnNumber.reduce(:+)
-
-                puts (validate % 10).zero?, luhnNumber, cleanNumber
-                return (validate % 10).zero?
+                
             end
-            
+            validate = luhnNumber.reduce(:+)
+
+                return (validate % 10).zero?
     end
 end
 end
