454577ab798f1e95d193d2b80ca8c0744e919482
Karla Álvarez <karla.alvarez@michelada.io>
2020-03-31 23:25:46 -0600

    Solve exercise, pass unit test

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,8 +1,30 @@
-=begin
-Write your code for the 'Clock' exercise in this file. Make the tests in
-`clock_test.rb` pass.
+class Clock
+    attr_reader :hour, :minute
+    def initialize(hour:0, minute:0)
+        @hour = hour
+        @minute = minute
+        @totalSeconds= (@hour*60 + @minute)* 60
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/clock` directory.
-=end
+    def to_s
+        Time.at(@totalSeconds).utc.strftime("%H:%M") #=> "01:00:00    
+    end
 
+    def +(newClock)
+        Clock.new(
+            hour: @hour + newClock.hour,
+            minute: @minute + newClock.minute
+        )
+    end
+
+    def -(newClock)
+        Clock.new(
+            hour: @hour - newClock.hour,
+            minute: @minute - newClock.minute
+        )
+    end
+
+    def ==(newClock)
+        self.to_s == newClock.to_s
+    end
+end
