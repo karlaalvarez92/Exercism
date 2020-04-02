fa0778093a591a07835769b0ec7909c228a89da7
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-02 17:20:05 -0600

    Solve exercise, pass mini test

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,7 @@
-=begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
-=end
-
+class Gigasecond
+    GIGASECOND = 10 **9;
+        
+    def self.from(date)
+       Time.at(date.to_i + GIGASECOND)
+    end
+end
