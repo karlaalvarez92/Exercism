73363d5395ca3f47d7760e62bb98233e8de803d0
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-01 18:21:11 -0600

    Solve exercise, pass mini test

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+    def initialize(number)
+        @number = number
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+    def square_of_sum
+        (1..@number).to_a.sum**2 
+       
+    end
+
+    def sum_of_squares
+        (1..@number).to_a.map{|el| el **2}.sum
+    end
+
+    def difference
+        square_of_sum - sum_of_squares
+    end
+end
 
