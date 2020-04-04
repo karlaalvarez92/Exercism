51e46c68ad222d3d4fdd79cb6fa9a32867cd54b6
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-03 19:59:35 -0600

    Solve exercise, pass mini test

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,8 +1,28 @@
-=begin
-Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
-`roman_numerals_test.rb` pass.
+class Fixnum
+    ROMAN =  {
+        1000 => "M",  
+        900  => "CM",  
+        500  => "D",  
+        400  => "CD",
+        100  => "C",  
+        90   => "XC",  
+        50   => "L",  
+        40   => "XL",  
+        10   => "X",  
+        9    => "IX",  
+        5    => "V",  
+        4    => "IV",  
+        1    => "I"  
+      }
+    def to_roman
+        arabic_number= self
+        roman_number= ""
 
-To get started with TDD, see the `README.md` file in your
-`ruby/roman-numerals` directory.
-=end
+        ROMAN.each do |number, letter|
+            roman_number << letter * (arabic_number / number)
+            arabic_number = arabic_number % number
+        end
+        roman_number
+    end
+end
 
