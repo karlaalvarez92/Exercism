f502ef1dd7be8b768d379a74abe4092c58be6db0
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-06 17:32:43 -0500

    Iterate code

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,6 +1,6 @@
 class Year
     def self.leap?(year)
-        (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        (year % 4.zero? && !year % 100.zero?) || year % 400.zero?;
 
     end
 end
