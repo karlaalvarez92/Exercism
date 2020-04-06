274d94be78a26584e69c7170e33d99b11a66e867
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-05 21:29:57 -0500

    Iterate code, change  chars method by digits method

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,8 +1,7 @@
 class ArmstrongNumbers
     def self.include?(number)
-        numbersArray = number.to_s.chars.map{|el| el.to_i}
-        calculated = numbersArray.map{|el| el ** numbersArray.length}.sum
-        calculated == number
+        numbers= number.digits
+        numbers.map{|n| n ** numbers.length}.sum == number
     end
 end
 
