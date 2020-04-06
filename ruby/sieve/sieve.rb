28f08e3c797c6e033b80a2c607a5cce9fbf0a301
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-05 21:01:17 -0500

    Solve exercise, pass mini test

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,21 +1,23 @@
 class Sieve
     def initialize(number)
-        @number=number       
+        @number=number 
+        @prime_numbers=[]      
     end
 
     def primes
-        prime_numbers=[]
-        current_number=1
-        
-        while current_number<= @number
-            i=0
-            (1..@number).each{|n| i+=1 if current_number % n == 0}
-            prime_numbers << current_number if i == 2
+        current_number=2
+
+        while current_number <= @number     
+            @prime_numbers << current_number if prime?(current_number)
             current_number+=1   
         end
 
-        prime_numbers
-        
+        @prime_numbers 
+    end
+
+    def prime?(number)
+        return true if @prime_numbers.empty?
+        @prime_numbers.each{|n|return false if number % n == 0}
     end
 end
 
