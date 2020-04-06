13b7dcf6449260119365d8578d5dc7b6e453bf7d
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-05 22:36:42 -0500

    Iterate code

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,6 +1,4 @@
 class Robot
-    @letters = ('AA'..'ZZ').to_a
-    @numbers = (0..999).to_a.map {|num| num.to_s.rjust(3, "0")}
 
     attr_reader :name
 
@@ -9,7 +7,7 @@ class Robot
     end
 
     def self.forget
-        @@all_names = @letters.map {|two_letters| @numbers.map {|three_numbers| two_letters + three_numbers} }.flatten.shuffle
+        @@all_names= ('AA000'..'ZZ999').to_a.shuffle
     end
 
     def reset
