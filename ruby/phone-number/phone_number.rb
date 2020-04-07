9b9ca680ef7c551f794f0607abe15230c9c15557
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-07 01:05:43 -0500

    Solve exercise, pass mini test

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,8 +1,24 @@
-=begin
-Write your code for the 'Phone Number' exercise in this file. Make the tests in
-`phone_number_test.rb` pass.
+class PhoneNumber
+    def self.clean(phone_number)
+    
+    clean_phone_number = phone_number.gsub(/\D/, "")
+    
+    #valid_phone_number? 
+    return nil if clean_phone_number.length < 10 || clean_phone_number.length > 11
 
-To get started with TDD, see the `README.md` file in your
-`ruby/phone-number` directory.
-=end
+    #valid_10_phone_number
+    return nil if clean_phone_number.length == 10 && 
+                  (clean_phone_number[0].match(/[^2-9]/) || 
+                  clean_phone_number[3].match(/[^2-9]/))
+
+    #valid_11_phone_number
+    return nil if clean_phone_number.length == 11 && 
+                  (clean_phone_number[0].match(/[^1]/) || 
+                  clean_phone_number[1].match(/[^2-9]/) ||
+                  clean_phone_number[4].match(/[^2-9]/))
+    return clean_phone_number.sub("1", "")   
+
+    clean_phone_number
+    end
+end
 
