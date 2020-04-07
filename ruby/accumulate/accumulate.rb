a764bb538866c4cfbf5c41497672f40bd03211ba
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-07 16:31:22 -0500

    Solve exercise, pass mini test

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,8 +1,10 @@
-=begin
-Write your code for the 'Accumulate' exercise in this file. Make the tests in
-`accumulate_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/accumulate` directory.
-=end
-
+class Array
+    
+    def accumulate(&block)
+        result=[]
+        for value in self
+            result << block.call(value)
+        end
+        result
+    end
+end
