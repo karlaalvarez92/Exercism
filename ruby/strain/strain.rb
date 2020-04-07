1a1ebcff252a90e69274506fb8e4fbc9bd30fe48
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-07 16:59:11 -0500

    Solve exercise, pass mini test

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Strain' exercise in this file. Make the tests in
-`strain_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/strain` directory.
-=end
+class Array 
+    def keep(&block)
+        result=[]
+        for value in self
+            result << value if yield(value)
+            
+        end
+        result
+    end
+    
+    def discard(&block)
+        result=[]
+        for value in self
+            result << value if !yield(value)
+        end
+        result
+    end
+end
 
