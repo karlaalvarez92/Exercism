ec74ae1689eb7cbc5e07f08db0a09d21981bff52
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-08 14:25:44 -0500

    Update code, create methods

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,11 +2,22 @@ class Bob
     def self.hey(remark)
         remark = remark.gsub(/\s+/, "")
         return "Fine. Be that way!" if remark == ""
-        return "Calm down, I know what I'm doing!" if remark.match(/[A-Z]/) && remark.upcase == remark && remark.chars.last== "?"
-        return "Whoa, chill out!" if remark.match(/[A-Z]/) && remark.upcase == remark 
-        return "Sure." if remark.chars.last== "?"
+        return "Calm down, I know what I'm doing!" if all_upcase?(remark) && question_mark?(remark)
+        return "Whoa, chill out!" if all_upcase?(remark)
+        return "Sure." if question_mark?(remark)
         return "Whatever."
     end
+
+    def self.all_upcase?(remark)
+        remark.match(/[A-Z]/) && remark.upcase == remark 
+    end
+
+    def self.question_mark?(remark)
+        remark.chars.last== "?"
+    end
 end
+
+#Separar por funciones 
+#Hacerlo orientado a objetos
 
 
