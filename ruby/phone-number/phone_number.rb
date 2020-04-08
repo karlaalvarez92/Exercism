96a525e69d281bb920d7551514645c141f0307fd
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-08 14:24:11 -0500

    Update code, create methods

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,23 +1,32 @@
 class PhoneNumber
+    VALID_LENGTH_10 =10
+    VALID_LENGTH_11 =11
+    COUNTRY_CODE = "1"
+    AREA_CODE= /[2-9]/
+
     def self.clean(phone_number)
-    
-    clean_phone_number = phone_number.gsub(/\D/, "")
-    
-    #valid_phone_number? 
-    return if clean_phone_number.length < 10 || clean_phone_number.length > 11
+        clean_phone_number = phone_number.gsub(/\D/, "")
 
-    #valid_10_phone_number
-    return if clean_phone_number.length == 10 && 
-                  (clean_phone_number[0].match(/[^2-9]/) || 
-                  clean_phone_number[3].match(/[^2-9]/))
-
-    #valid_11_phone_number
-    return if clean_phone_number.length == 11 && 
-                  (clean_phone_number[0].match(/[^1]/) || 
-                  clean_phone_number[1].match(/[^2-9]/) ||
-                  clean_phone_number[4].match(/[^2-9]/))
-    return clean_phone_number.sub("1", "")   
-
-    clean_phone_number
+        return if !valid_phone_number?(clean_phone_number)
+        return clean_phone_number.sub("1", "") if valid_11_phone_number?(clean_phone_number)
+        return if !valid_10_phone_number?(clean_phone_number)
+        clean_phone_number
     end
+
+    def self.valid_phone_number?(number)
+        number.length == VALID_LENGTH_10 || number.length == VALID_LENGTH_11
+    end
+
+    def self.valid_10_phone_number?(number)
+        number.length == 10 && 
+        number[0].match(AREA_CODE) && 
+        number[3].match(AREA_CODE)
+    end
+
+     def self.valid_11_phone_number?(number)
+        number.length == 11 && 
+        number[0]== COUNTRY_CODE && 
+        number[1].match(AREA_CODE) &&
+        number[4].match(AREA_CODE)
+     end
 end
