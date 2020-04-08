feea1bc9d182330363f39fe5d500fe25f6f2310d
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-07 19:30:22 -0500

    Solve exercise, pass mini test

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,8 +1,30 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+class SpaceAge
+    YEAR_IN_PLANETS = {
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        earth: 1,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+        }
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+    YEAR_IN_SECONDS = 31557600.0;
+
+    attr_reader :age_in_seconds
+
+    def initialize(seconds)
+        @age_in_seconds = seconds
+      end
+     
+    
+        YEAR_IN_PLANETS.each{ |planet, years|
+        define_method("on_#{planet}"){age_in_seconds/(YEAR_IN_SECONDS * years)}  
+      }    
+
+end
+
+
+
 
