5de5e7dc438f15303d5a7eb81e28b668a3ea65f8
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-08 15:36:50 -0500

    Solve exercise, pass mini test

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,8 +1,26 @@
-=begin
-Write your code for the 'Atbash Cipher' exercise in this file. Make the tests in
-`atbash_cipher_test.rb` pass.
+class Atbash
+    ALPHABET = ("a".."z").to_a
+    CIPHER = ALPHABET.reverse
 
-To get started with TDD, see the `README.md` file in your
-`ruby/atbash-cipher` directory.
-=end
+    def self.encode(plaintext)
+        array_text= plaintext.downcase.gsub(/[\s,]/, "").chars
+        ciphertext = []
+        array_text.each{|letter| 
+        letter.match?(/[a-z]/) ? ciphertext << CIPHER[ALPHABET.index(letter)] :
+        ciphertext << letter  
+    }
+    ciphertext.count >= 5 ? ciphertext.join.scan(/\w{1,5}/).join(" ") : 
+    ciphertext.join
+    end
+
+    def self.decode(ciphertext)
+        array_cipher= ciphertext.delete(" ").chars
+        plaintext = []
+        array_cipher.each{|letter| 
+        letter.match?(/[a-z]/) ? plaintext << ALPHABET[CIPHER.index(letter)] :
+        plaintext << letter  
+    }
+    plaintext.join
+    end
+end
 
