29e32fa0c4ce97e1dbfb402c6953c5bf33662e48
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-09 01:46:11 -0500

    Solve exercise, pass mini test

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,8 +1,27 @@
-=begin
-Write your code for the 'Crypto Square' exercise in this file. Make the tests in
-`crypto_square_test.rb` pass.
+class Crypto
+  def initialize(plaintext)
+      @plaintext=plaintext.gsub(/[\s@,.%\!]/, "").downcase
+  end
+  
+  def ciphertext
+      return @plaintext if @plaintext.length < 3
 
-To get started with TDD, see the `README.md` file in your
-`ruby/crypto-square` directory.
-=end
+      #ESTAS VAN A SER LAS COLUMNAS FINALES, CEIL PARA REDONDEAR HACIA ARRIBA
+      columns = Math.sqrt(@plaintext.length).ceil
+      #EN ESTE NO ME FUNCIONÓ CEIL POR ESO AGREGÉ LA LINEA 13 PARA CORREGIR CIERTOS CASOS
+      raws = (@plaintext.length/columns)
+      raws+=1 if @plaintext.length > raws*columns
+      #ESTO ES PARA AGREGAR LOS ESPACIOS EN BLANCO EN CASO QUE EL LAS LETRAS NO LLENEN LA CANTIDAD NECESARIA
+      @plaintext+= " "*(columns*raws-@plaintext.length)
+      
+      array = []
+      i=0
 
+      while array.length < raws
+      array << @plaintext.chars.slice(i, columns)
+      i+= columns
+      end
+
+      array.transpose.map{|el| el.join}.join(" ")      
+  end
+end
