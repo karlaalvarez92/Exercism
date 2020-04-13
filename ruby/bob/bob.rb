5656d023f20961f89ac9176296b1d4564a7a1f2d
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 10:46:29 -0500

    Update methods names, add empty method, correct linter

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,23 +1,27 @@
 class Bob
-    def self.hey(remark)
-        remark = remark.gsub(/\s+/, "")
-        return "Fine. Be that way!" if remark == ""
-        return "Calm down, I know what I'm doing!" if all_upcase?(remark) && question_mark?(remark)
-        return "Whoa, chill out!" if all_upcase?(remark)
-        return "Sure." if question_mark?(remark)
-        return "Whatever."
-    end
+  def self.hey(remark)
+    remark = remark.gsub(/\s+/, "")
 
-    def self.all_upcase?(remark)
-        remark.match(/[A-Z]/) && remark.upcase == remark 
-    end
+    return "Fine. Be that way!" if empty?(remark)
+    return "Calm down, I know what I'm doing!" if shout?(remark) && question?(remark)
+    return "Whoa, chill out!" if shout?(remark)
+    return "Sure." if question?(remark)
+    return "Whatever."
+  end
 
-    def self.question_mark?(remark)
-        remark.chars.last== "?"
-    end
+  def self.empty?(remark)
+    remark.empty?
+  end
+
+  def self.shout?(remark)
+    remark.match(/[A-Z]/) && remark.upcase == remark 
+  end
+
+  def self.question?(remark)
+    remark.chars.last== "?"
+  end
 end
 
-#Separar por funciones 
-#Hacerlo orientado a objetos
+
 
 
