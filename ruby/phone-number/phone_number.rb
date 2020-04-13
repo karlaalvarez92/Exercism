5c95c4d76d5c1a7dd8ed24b7850ad12635a9bfa1
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 18:45:20 -0500

    Iterate exercise

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,32 +1,24 @@
+# Clase
+#
+# frozen_string_literal: true
+
 class PhoneNumber
-    VALID_LENGTH_10 =10
-    VALID_LENGTH_11 =11
-    COUNTRY_CODE = "1"
-    AREA_CODE= /[2-9]/
+  def self.clean(phone_number)
+    clean_phone_number = phone_number.gsub(/^\+?1|\D/, '')
 
-    def self.clean(phone_number)
-        clean_phone_number = phone_number.gsub(/\D/, "")
+    @country_code = clean_phone_number[0].to_i
+    @area_code = clean_phone_number[3].to_i
 
-        return if !valid_phone_number?(clean_phone_number)
-        return clean_phone_number.sub("1", "") if valid_11_phone_number?(clean_phone_number)
-        return if !valid_10_phone_number?(clean_phone_number)
-        clean_phone_number
-    end
+    return if !valid_size?(clean_phone_number) || !valid_codes?
 
-    def self.valid_phone_number?(number)
-        number.length == VALID_LENGTH_10 || number.length == VALID_LENGTH_11
-    end
+    clean_phone_number
+  end
 
-    def self.valid_10_phone_number?(number)
-        number.length == 10 && 
-        number[0].match(AREA_CODE) && 
-        number[3].match(AREA_CODE)
-    end
+  def self.valid_size?(clean_phone_number)
+    clean_phone_number.size == 10
+  end
 
-     def self.valid_11_phone_number?(number)
-        number.length == 11 && 
-        number[0]== COUNTRY_CODE && 
-        number[1].match(AREA_CODE) &&
-        number[4].match(AREA_CODE)
-     end
+  def self.valid_codes?
+    @country_code > 1 && @area_code > 1
+  end
 end
