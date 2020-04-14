e536501e2db38f758315ea7f2c2fea54233fb4fe
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 19:16:58 -0500

    Remove innecesary code

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,22 +1,19 @@
-require "prime"
+# frozen_string_literal: true
 
+# Class PrimeFactors
+#
 class PrimeFactors
-   def self.of(limit)
-      prime_factors = []
-      i = 2
-    
-      while i <= limit 
-         quotient = limit / i
-    
-         if limit % i == 0 
-            limit = quotient
-            prime_factors << i
-         else 
-            i+=1
-        end
+  def self.of(limit)
+    prime_factors = []
+    i = 2
+    while i <= limit
+      if (limit % i).zero?
+        limit /= i
+        prime_factors << i
+      else
+        i += 1
       end
-      
-      prime_factors
-   end
+    end
+    prime_factors
+  end
 end
-
