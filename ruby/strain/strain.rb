f583f39b18cc589cd4272b3452259a765895f29b
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 20:00:22 -0500

    Add frozen_string_literal: true for rubocop

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,19 +1,21 @@
-class Array 
-    def keep(&block)
-        result=[]
-        for value in self
-            result << value if yield(value)
-            
-        end
-        result
-    end
-    
-    def discard(&block)
-        result=[]
-        for value in self
-            result << value if !yield(value)
-        end
-        result
-    end
-end
+# frozen_string_literal: true
 
+# Class Array
+#
+class Array
+  def keep
+    result = []
+    each do |value|
+      result << value if yield(value)
+    end
+    result
+  end
+
+  def discard
+    result = []
+    each do |value|
+      result << value unless yield(value)
+    end
+    result
+  end
+end
