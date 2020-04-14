ccc0415c6025f66981e5f35464fcdf34d36dad9b
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 21:53:23 -0500

    Add frozen_string_literal: true for rubocop

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,9 +1,13 @@
-class FlattenArray
-    def self.flatten(compound_array, flatten_array=[])
-        compound_array.each { |el| 
-        el.is_a?(Array) ? flatten(el, flatten_array) : flatten_array << el } 
-        
-        flatten_array.compact
-    end
-end
+# frozen_string_literal: true
 
+# Class FlattenArray
+#
+class FlattenArray
+  def self.flatten(compound_array, flatten_array = [])
+    compound_array.each do |el|
+      el.is_a?(Array) ? flatten(el, flatten_array) : flatten_array << el
+    end
+
+    flatten_array.compact
+  end
+end
