94a7e340d4ddb0a883dd308da7576c1ed6374989
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 19:34:17 -0500

    Add frozen_string_literal: true for rubocop

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,23 +1,26 @@
+# frozen_string_literal: true
+
+# Class BeerSong
+#
 class BeerSong
-    
+  def self.recite(start, finish)
+    bottles = 'bottles'
 
-    def self.recite(start, finish)
-        bottles = "bottles"
+    penultimate_strophe = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    final_strophe = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
 
-        penultimate_strophe = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        final_strophe = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-   
-        lyrics= [] 
+    lyrics = []
 
-        start.downto(start-finish+1){ |n| 
-        strophe =
-        bottles = "bottle" if n - 1 == 1
+    start.downto(start - finish + 1) do |n|
+      bottles = 'bottle' if n - 1 == 1
 
-        lyrics << "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} #{bottles} of beer on the wall.\n" if n > 1
-        lyrics << penultimate_strophe if n == 1
-        lyrics << final_strophe if n == 0 
-    }
-        
-     lyrics.join("\n")
+      if n > 1
+        lyrics << "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n - 1} #{bottles} of beer on the wall.\n"
+      end
+      lyrics << penultimate_strophe if n == 1
+      lyrics << final_strophe if n == 0
     end
+
+    lyrics.join("\n")
+  end
 end
