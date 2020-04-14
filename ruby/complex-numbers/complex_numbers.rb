6c7c1752ff115314b2045df73087ce2f4eb2edb7
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-14 18:10:12 -0500

    Solve exercise, pass mini test

--- ruby/complex-numbers/complex_numbers.rb
+++ ruby/complex-numbers/complex_numbers.rb
@@ -1,8 +1,63 @@
-=begin
-Write your code for the 'Complex Numbers' exercise in this file. Make the tests in
-`complex_numbers_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/complex-numbers` directory.
-=end
+# Class ComplexNumber
+#
+class ComplexNumber
+  attr_reader :real, :imaginary
+  def initialize(real, imaginary)
+    @real = 0.0 + real
+    @imaginary = 0.0 + imaginary
+  end
 
+  def ==(other)
+    real == other.real && imaginary == other.imaginary
+  end
+
+  def +(other)
+    @real += other.real
+    @imaginary += other.imaginary
+    self
+  end
+
+  def -(other)
+    @real -= other.real
+    @imaginary -= other.imaginary
+    self
+  end
+
+  def *(other)
+    new_real = @real * other.real - @imaginary * other.imaginary
+
+    @imaginary = @imaginary * other.real + @real * other.imaginary
+    @real = new_real
+    self
+  end
+
+  def /(other)
+    new_real =
+      (@real * other.real + @imaginary * other.imaginary) /
+      (other.real**2 + other.imaginary**2)
+
+    @imaginary =
+      (@imaginary * other.real - @real * other.imaginary) /
+      (other.real**2 + other.imaginary**2)
+    @real = new_real
+    self
+  end
+
+  def abs
+    Math.sqrt(@real**2 + @imaginary**2)
+  end
+
+  def conjugate
+    @imaginary *= -1 unless @imaginary.zero?
+    self
+  end
+
+  def exp
+    new_real = Math.exp(@real) * Math.cos(@imaginary)
+    @imaginary = Math.exp(@real) * Math.sin(@imaginary).floor
+    @real = new_real
+    self
+  end
+end
