e0360c16ff6b28ae65e859ecd61c89ff900470ee
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 20:02:05 -0500

    Add frozen_string_literal: true for rubocop

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,31 +1,28 @@
-require "date"
-class Meetup
-    attr_reader :month, :year
+require 'date'
+# frozen_string_literal: true
 
-    COUNTER = {
+# Class Meetup
+#
+class Meetup
+  attr_reader :month, :year
+
+  COUNTER = {
     first: 1,
     second: 8,
     third: 15,
     fourth: 22,
     teenth: 13,
     last: -7
-  }
+  }.freeze
 
-    def initialize(month, year)
-        @month = month
-        @year = year
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
 
-    end
-    def day(weekday, counting  )
-    
-        date = Date.new(year, month, COUNTER[counting] )
-        date = date.next until date.send("#{weekday}?")
-        date
-        
-    end
+  def day(weekday, counting)
+    date = Date.new(year, month, COUNTER[counting])
+    date = date.next until date.send("#{weekday}?")
+    date
+  end
 end
-
-#d = Date.parse('3rd Feb 2001')
-#d.strftime('%a %d %b %Y')
-
-
