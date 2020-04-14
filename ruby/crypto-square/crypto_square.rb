9ac4461b32c6686630983b1a55ec7df47b8ba61d
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 21:46:15 -0500

    Iterate code, add frozen_string_literal: true for rubocop

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,27 +1,22 @@
+# frozen_string_literal: true
+
+# Class Crypto
+#
 class Crypto
-  def initialize(plaintext)
-      @plaintext=plaintext.gsub(/[\s@,.%\!]/, "").downcase
+  def initialize(text)
+    @plaintext = text.to_s.gsub(/[\s@,.%\!]/, '').downcase
+    puts "#{text} => #{@plaintext}"
   end
-  
+
   def ciphertext
-      return @plaintext if @plaintext.length < 3
+    columns = Math.sqrt(@plaintext.length).ceil
+    return '' if columns.zero?
 
-      #ESTAS VAN A SER LAS COLUMNAS FINALES, CEIL PARA REDONDEAR HACIA ARRIBA
-      columns = Math.sqrt(@plaintext.length).ceil
-      #EN ESTE NO ME FUNCIONÓ CEIL POR ESO AGREGÉ LA LINEA 13 PARA CORREGIR CIERTOS CASOS
-      raws = (@plaintext.length/columns)
-      raws+=1 if @plaintext.length > raws*columns
-      #ESTO ES PARA AGREGAR LOS ESPACIOS EN BLANCO EN CASO QUE EL LAS LETRAS NO LLENEN LA CANTIDAD NECESARIA
-      @plaintext+= " "*(columns*raws-@plaintext.length)
-      
-      array = []
-      i=0
-
-      while array.length < raws
-      array << @plaintext.chars.slice(i, columns)
-      i+= columns
-      end
-
-      array.transpose.map{|el| el.join}.join(" ")      
+    code_text = []
+    @plaintext.chars.each_slice(columns) do |slice|
+      slice << ' ' while slice.length < columns
+      code_text << slice
+    end
+    code_text.transpose.map(&:join).join(' ')
   end
 end
