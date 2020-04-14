c9694f1e544eb7cb955eb90fc36aa2f55f60ca02
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-14 09:59:09 -0500

    Iterate exercise

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,26 +1,20 @@
+# frozen_string_literal: true
+
+# Class Atbash
+#
 class Atbash
-    ALPHABET = ("a".."z").to_a
-    CIPHER = ALPHABET.reverse
+  CIPHER_ALPHABET = Hash[('a'..'z').zip(('a'..'z').to_a.reverse)]
 
-    def self.encode(plaintext)
-        array_text= plaintext.downcase.gsub(/[\s,]/, "").chars
-        ciphertext = []
-        array_text.each{|letter| 
-        letter.match?(/[a-z]/) ? ciphertext << CIPHER[ALPHABET.index(letter)] :
-        ciphertext << letter  
-    }
-    ciphertext.count >= 5 ? ciphertext.join.scan(/\w{1,5}/).join(" ") : 
-    ciphertext.join
-    end
+  def self.encode(plaintext)
+    plaintext.downcase.chars.map { |char| cipher(char) }
+             .join.gsub(/(.{5})/, '\1 ').strip
+  end
 
-    def self.decode(ciphertext)
-        array_cipher= ciphertext.delete(" ").chars
-        plaintext = []
-        array_cipher.each{|letter| 
-        letter.match?(/[a-z]/) ? plaintext << ALPHABET[CIPHER.index(letter)] :
-        plaintext << letter  
-    }
-    plaintext.join
-    end
+  def self.decode(cyphertext)
+    cyphertext.downcase.chars.map { |char| cipher(char) }.join
+  end
+
+  def self.cipher(character)
+    CIPHER_ALPHABET.fetch(character) { |char| char.match?(/\d/) ? char : nil }
+  end
 end
-
