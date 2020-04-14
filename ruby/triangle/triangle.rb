f2434bb4454e8b9f506b4c867a06722bc20de681
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-13 22:09:02 -0500

    Add frozen_string_literal: true for rubocop

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,25 +1,28 @@
+# frozen_string_literal: true
+
+# Class Triangle
+#
 class Triangle
-    attr_reader :sides
+  attr_reader :sides
 
-    def initialize(sides)
-      @sides = sides
-    end
-  
-    def equilateral?
-      triangle? && sides.uniq.count == 1
-    end
-  
-    def isosceles?
-      triangle? && sides.uniq.count <= 2
-    end
-  
-    def scalene?
-      triangle? && sides.uniq.count == 3
-    end
+  def initialize(sides)
+    @sides = sides
+  end
 
-    def triangle? 
-       @sides.all? { |side| side > 0} && 
-       @sides.permutation(3).all? { |(a,b,c)| a <= b + c }
-    end
+  def equilateral?
+    triangle? && sides.uniq.count == 1
+  end
+
+  def isosceles?
+    triangle? && sides.uniq.count <= 2
+  end
+
+  def scalene?
+    triangle? && sides.uniq.count == 3
+  end
+
+  def triangle?
+    @sides.all?(&:positive?) &&
+      @sides.permutation(3).all? { |(a, b, c)| a <= b + c }
+  end
 end
-  
