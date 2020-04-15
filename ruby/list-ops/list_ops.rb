444b8797101bfdc5d131c873b5a86c93d39cfd15
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-15 14:46:28 -0500

    Solve exercise, pass mini test

--- ruby/list-ops/list_ops.rb
+++ ruby/list-ops/list_ops.rb
@@ -1,8 +1,54 @@
-=begin
-Write your code for the 'List Ops' exercise in this file. Make the tests in
-`list_ops_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/list-ops` directory.
-=end
+# Class ListOps
+#
+class ListOps
+  def self.arrays(array)
+    counter = 0
+    array.each { |el| counter += 1 if el.nil? }
+    counter
+  end
 
+  def self.reverser(array)
+    reverse = []
+    reverse << array.pop until array.length.zero?
+    reverse
+  end
+
+  def self.concatter(input1, input2)
+    compound_array = [input1, input2]
+    concat_array = []
+    compound_array.each do |array|
+      array.each { |el| concat_array << el }
+    end
+    concat_array
+  end
+
+  def self.mapper(array)
+    result = []
+    array.each do |el|
+      result << yield(el)
+    end
+    result
+  end
+
+  def self.filterer(array)
+    result = []
+    array.each do |el|
+      result << el if yield(el)
+    end
+    result
+  end
+
+  def self.sum_reducer(array)
+    result = 0
+    array.each { |num| result += num }
+    result
+  end
+
+  def self.factorial_reducer(array)
+    result = 1
+    array.each { |num| result *= num }
+    result
+  end
+end
--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
+# Class Pangram
+#
 class Pangram
-    ABC = ('a'..'z')
-    def self.pangram?(sentence)
-        ABC.all?{|el| sentence.downcase.include?(el)}
-    end
+  ABC = ('a'..'z').freeze
+  def self.pangram?(sentence)
+    ABC.all? { |el| sentence.downcase.include?(el) }
+  end
 end
 
+# ESTO TAMBIÉN PASA LAS PRUEBAS PERFECTAMENTE
+# class Pangram
+# ABC = ('a'..'z').to_a
+# def self.pangram?(sentence)
+# (ABC - sentence.downcase.chars).empty?
+# end
+# end
