aea0622061fec24a52bc7c80366cfff98207f45e
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-16 15:16:09 -0500

    Solve exercise, pass mini test

--- ruby/largest-series-product/largest_series_product.rb
+++ ruby/largest-series-product/largest_series_product.rb
@@ -1,8 +1,28 @@
-=begin
-Write your code for the 'Largest Series Product' exercise in this file. Make the tests in
-`largest_series_product_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/largest-series-product` directory.
-=end
+# Class Series
+#
+class Series
+  def initialize(number)
+    @number = number
+  end
 
+  def largest_product(size)
+    return 1 if size.zero?
+
+    valid_number?(size)
+    digits = @number.chars.map(&:to_i)
+
+    array = []
+    digits.each_cons(size) { |slice| array << slice.reduce(&:*) }
+    array.max
+  end
+
+  def valid_number?(size)
+    if @number.length < size ||
+       (@number == '' && size.positive?) ||
+       @number.match?(/\D/) || size.negative?
+      raise ArgumentError
+    end
+  end
+end
