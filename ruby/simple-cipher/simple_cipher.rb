210a8e2b25a87fa959a78df5a6dcfe748630f183
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-16 14:42:33 -0500

    Solve exercise, pass mini test

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -1,8 +1,53 @@
-=begin
-Write your code for the 'Simple Cipher' exercise in this file. Make the tests in
-`simple_cipher_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/simple-cipher` directory.
-=end
+# Class Cipher
+#
+class Cipher
+  attr_reader :key
 
+  KEYS = ('a'..'z').to_a
+
+  def initialize(key = default_key)
+    validate_key(key)
+    @key = key
+  end
+
+  def default_key
+    ('a'..'z').to_a.sample(100).join
+  end
+
+  def validate_key(key)
+    raise ArgumentError unless key.match(/^[a-z]+$/)
+  end
+
+  def encode(plaintext)
+    plaintext = plaintext.chars
+    keys = @key.chars.slice(0, plaintext.length)
+
+    encode = ''
+    plaintext.each_with_index do |letter, index|
+      new_index = position_of(keys[index]) + position_of(letter)
+      new_index > 25 ? encode_letter = KEYS[new_index - 26] : encode_letter = KEYS[new_index]
+      encode += encode_letter
+    end
+    encode
+  end
+
+  def decode(ciphertext)
+    ciphertext = ciphertext.chars
+    keys = @key.chars.slice(0, ciphertext.length)
+
+    decode = ''
+    ciphertext.each_with_index do |letter, index|
+      new_index = position_of(letter) - position_of(keys[index])
+      new_index.negative? ? decode_letter = KEYS[26 + new_index] : decode_letter = KEYS[new_index]
+
+      decode += decode_letter
+    end
+    decode
+  end
+
+  def position_of(letter)
+    KEYS.index(letter)
+  end
+end
