da2bc85a0f8181128cd51029f8f1dbf1085364f3
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-16 16:57:01 -0500

    Solve exercise, pass mini test

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,8 +1,29 @@
-=begin
-Write your code for the 'Rotational Cipher' exercise in this file. Make the tests in
-`rotational_cipher_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/rotational-cipher` directory.
-=end
+class RotationalCipher
+  LETTERS = ('a'..'z').to_a
+  CAPITAL_LETTERS = ('A'..'Z').to_a
 
+  def self.rotate(plaintext, n)
+    ciphertext = ''
+
+    plaintext.each_char do |letter|
+      if letter.match?(/[A-Z]/)
+        original_index = CAPITAL_LETTERS.index(letter)
+        new_order = CAPITAL_LETTERS.rotate(n)
+        ciphertext += new_order[original_index]
+
+      elsif letter.match?(/[a-z]/)
+        original_index = LETTERS.index(letter)
+        new_order = LETTERS.rotate(n)
+        new_order[original_index]
+        ciphertext += new_order[original_index]
+
+      else
+        ciphertext += letter
+      end
+    end
+
+    ciphertext
+  end
+end
