e41d07732a6c52704e823959e19f70ca9d55ffea
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-17 09:23:38 -0500

    Solve exercise, pass mini test

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -1,8 +1,36 @@
-=begin
-Write your code for the 'Protein Translation' exercise in this file. Make the tests in
-`protein_translation_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/protein-translation` directory.
-=end
+InvalidCodonError = Class.new(StandardError)
 
+class Translation
+  PROTEINS = {
+    Methionine: %w[AUG],
+    Phenylalanine: %w[UUU UUC],
+    Leucine: %w[UUA UUG],
+    Serine: %w[UCU UCC UCA UCG],
+    Tyrosine: %w[UAU UAC],
+    Cysteine: %w[UGU UGC],
+    Tryptophan: %w[UGG],
+    STOP: %w[UAA UAG UGA]
+  }.freeze
+
+  def self.of_codon(codon)
+    proteins = ''
+    PROTEINS.each { |key, value| proteins = key.to_s if value.include?(codon) }
+    proteins
+  end
+
+  def self.of_rna(strand)
+    proteins = []
+    strand.chars.each_slice(3) do |slice|
+      PROTEINS.each do |key, value|
+        return proteins if %w[UAA UAG UGA].include?(slice.join)
+
+        proteins << key.to_s if value.include?(slice.join)
+      end
+    end
+    raise InvalidCodonError if proteins.empty?
+
+    proteins
+  end
+end
--- ruby/protein-translation/protein_translation_test.rb
+++ ruby/protein-translation/protein_translation_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative 'protein_translation'
 
@@ -7,74 +9,74 @@ class TranslationTest < Minitest::Test
   end
 
   def test_identifies_Phenylalanine_codons
-    skip
+    # skip
     assert_equal 'Phenylalanine', Translation.of_codon('UUU')
     assert_equal 'Phenylalanine', Translation.of_codon('UUC')
   end
 
   def test_identifies_Leucine_codons
-    skip
-    %w(UUA UUG).each do |codon|
+    # skip
+    %w[UUA UUG].each do |codon|
       assert_equal 'Leucine', Translation.of_codon(codon)
     end
   end
 
   def test_identifies_Serine_codons
-    skip
-    %w(UCU UCC UCA UCG).each do |codon|
+    # skip
+    %w[UCU UCC UCA UCG].each do |codon|
       assert_equal 'Serine', Translation.of_codon(codon)
     end
   end
 
   def test_identifies_Tyrosine_codons
-    skip
-    %w(UAU UAC).each do |codon|
+    # skip
+    %w[UAU UAC].each do |codon|
       assert_equal 'Tyrosine', Translation.of_codon(codon)
     end
   end
 
   def test_identifies_Cysteine_codons
-    skip
-    %w(UGU UGC).each do |codon|
+    # skip
+    %w[UGU UGC].each do |codon|
       assert_equal 'Cysteine', Translation.of_codon(codon)
     end
   end
 
   def test_identifies_Tryptophan_codons
-    skip
+    # skip
     assert_equal 'Tryptophan', Translation.of_codon('UGG')
   end
 
   def test_identifies_stop_codons
-    skip
-    %w(UAA UAG UGA).each do |codon|
+    # skip
+    %w[UAA UAG UGA].each do |codon|
       assert_equal 'STOP', Translation.of_codon(codon)
     end
   end
 
   def test_translates_rna_strand_into_correct_protein
-    skip
+    # skip
     strand = 'AUGUUUUGG'
-    expected = %w(Methionine Phenylalanine Tryptophan)
+    expected = %w[Methionine Phenylalanine Tryptophan]
     assert_equal expected, Translation.of_rna(strand)
   end
 
   def test_stops_translation_if_stop_codon_present
-    skip
+    # skip
     strand = 'AUGUUUUAA'
-    expected = %w(Methionine Phenylalanine)
+    expected = %w[Methionine Phenylalanine]
     assert_equal expected, Translation.of_rna(strand)
   end
 
   def test_stops_translation_of_longer_strand
-    skip
+    # skip
     strand = 'UGGUGUUAUUAAUGGUUU'
-    expected = %w(Tryptophan Cysteine Tyrosine)
+    expected = %w[Tryptophan Cysteine Tyrosine]
     assert_equal expected, Translation.of_rna(strand)
   end
 
   def test_invalid_codons
-    skip
+    # skip
     strand = 'CARROT'
     assert_raises(InvalidCodonError) do
       Translation.of_rna(strand)
