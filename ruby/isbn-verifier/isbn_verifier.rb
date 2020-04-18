d052f95ecbe1e7ab3472d7a3198680801265e97c
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-18 03:09:03 -0500

    Solve exercise, pass mini test

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,8 +1,23 @@
-=begin
-Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
-`isbn_verifier_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/isbn-verifier` directory.
-=end
+class IsbnVerifier
+  def self.valid?(string)
+    array = string.gsub(/[^X0-9]/, '').chars
+    array[-1] = 10 if array.last == 'X'
 
+    return false unless array.length == 10 || array.include?(/[^A-Z]/)
+
+    validate(array)
+  end
+
+  def self.validate(array)
+    array = array.map(&:to_i)
+    acc = 0
+
+    array.each_with_index do |number, index|
+      acc += number * (index + 1)
+    end
+
+    (acc % 11).zero?
+  end
+end
