688314efed54915815256051dc3ed1de29ce8225
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-21 21:59:25 -0500

    Solve exercise, pass unit test

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Run Length Encoding' exercise in this file. Make the tests in
-`run_length_encoding_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/run-length-encoding` directory.
-=end
+# Class RunLengthEncoding
+#
+class RunLengthEncoding
+  def self.encode(input)
+    input.chars
+         .chunk { |el| el }
+         .collect do |letter, collection|
+      [collection.length > 1 ? collection.length : '', letter].join
+    end .join
+  end
 
+  def self.decode(input)
+    input.scan(/(\d*)([ \w]+?)/)
+         .map { |pair| pair[1] * (pair[0] != '' ? pair[0].to_i : 1) }
+         .join
+  end
+end
