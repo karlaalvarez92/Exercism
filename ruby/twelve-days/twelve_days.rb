59439bb15610c87a6eb622c175204d62e91e184a
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-23 02:02:47 -0500

    Solve exercise, pass unit test

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,8 +1,34 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+class TwelveDays
+  SONG = {
+    1 => ['first', "a Partridge in a Pear Tree.\n"],
+    2 => ['second', 'two Turtle Doves, and '],
+    3 => ['third', 'three French Hens, '],
+    4 => ['fourth', 'four Calling Birds, '],
+    5 => ['fifth', 'five Gold Rings, '],
+    6 => ['sixth', 'six Geese-a-Laying, '],
+    7 => ['seventh', 'seven Swans-a-Swimming, '],
+    8 => ['eighth', 'eight Maids-a-Milking, '],
+    9 => ['ninth', 'nine Ladies Dancing, '],
+    10 => ['tenth', 'ten Lords-a-Leaping, '],
+    11 => ['eleventh', 'eleven Pipers Piping, '],
+    12 => ['twelfth', 'twelve Drummers Drumming, ']
+  }.freeze
 
+  def self.song
+    line_break = "\n"
+    verse = ''
+    (1..12).each do |num|
+      line_break = '' if num == 12
+      verse += "On the #{SONG[num][0]} day of Christmas my true love gave to me: " + gifts(num) + line_break
+    end
+    verse
+  end
+
+  def self.gifts(start)
+    gifts = ''
+    start.downto(1) { |num| gifts += SONG[num][1] }
+    gifts
+  end
+end
