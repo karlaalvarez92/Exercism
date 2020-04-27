6712029dd6042aa74ce8aebfb268ef5ab762a663
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-27 10:05:24 -0500

    Iterate, solution

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -8,10 +8,8 @@ class Anagram
   end
 
   def match(words)
-    words.map do |word|
-      if word.downcase.chars.sort == @word.chars.sort && word.downcase != @word
-        word
-      end
-    end.compact
+    words.select do |word|
+      word.downcase.chars.sort == @word.chars.sort && word.downcase != @word
+    end
   end
 end
