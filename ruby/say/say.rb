531e618f99af0d42fd780c2d68307b1c97764d10
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-30 13:53:48 -0500

    Solve exercise, pass unit test

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -1,8 +1,44 @@
-=begin
-Write your code for the 'Say' exercise in this file. Make the tests in
-`say_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/say` directory.
-=end
+class Say
+  NUMBERS = {
+    1 => ['one', 'ten', ' one hundred'],
+    2 => ['two', ' twenty-', ' two hundred'],
+    3 => ['three', ' thirty-', ' three hundred'],
+    4 => ['four', ' forty-', ' four hundred', 'fourteen'],
+    5 => ['five', ' fifty-', ' five hundred'],
+    6 => ['six', ' sixty-', ' six hundred'],
+    7 => ['seven', ' seventy-', 'seventy hundred'],
+    8 => ['eight', ' eighty-', 'eight hundred'],
+    9 => ['nine', ' ninety-', 'nine hundred']
+  }.freeze
 
+  UNITS = 0
+  TEENS = 3
+
+  BIG_NUMBERS = ['', ' thousand ', ' million ', ' billion '].freeze
+
+  def initialize(number)
+    @number = number
+    raise ArgumentError if @number.negative? || @number > 999_999_999_999
+  end
+
+  def in_english
+    return 'zero' if @number.zero?
+    return NUMBERS[@number.digits[UNITS]][TEENS] if @number.between?(11, 19)
+
+    letters = []
+    @number.digits.each_slice(3).with_index do |slice, index|
+      next if slice.all?(&:zero?)
+
+      number = [BIG_NUMBERS[index]]
+      slice.each.with_index do |num, i|
+        next if num.zero?
+
+        number << NUMBERS[num][i]
+      end
+      letters << number
+    end
+    letters.map(&:reverse).reverse.compact.join.gsub(/  |-$/, ' ').strip
+  end
+end
