cb5e543799ff036a445d488a590298aad53e5337
Karla Álvarez <karla.alvarez@michelada.io>
2020-04-30 20:12:48 -0500

    Solve exercise, pass mini test

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -1,8 +1,51 @@
-=begin
-Write your code for the 'Rail Fence Cipher' exercise in this file. Make the tests in
-`rail_fence_cipher_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/rail-fence-cipher` directory.
-=end
+class RailFenceCipher
+  def self.encode(plaintext, rails)
+    rail_cipher(plaintext, rails).join
+  end
 
+  def self.decode(ciphertext, rails)
+    palabra = ciphertext.chars
+    algo = rail_cipher(ciphertext, rails)
+    new_algo = []
+    algo.each do |el|
+      new_algo << Array.new(el.length)
+    end
+    new_algo.map! do |array|
+      array.map! do |letter|
+        palabra.shift if letter.nil?
+      end
+    end
+
+    i = 0
+    upawrd = true
+    keac = []
+    until keac.length == ciphertext.chars.length
+      keac << new_algo[i].shift
+      upawrd ? i += 1 : i -= 1
+      upawrd = true if i == 0
+      upawrd = false if i == rails - 1
+    end
+    keac.join
+  end
+
+  private
+
+  def self.rail_cipher(text, rails)
+    result = []
+    i = 0
+    upawrd = true
+
+    until result.join.length == text.length
+      text.each_char do |letter|
+        result[i] ? result[i] << letter : result << Array.new(1, letter)
+
+        upawrd ? i += 1 : i -= 1
+        upawrd = true if i == 0
+        upawrd = false if i == rails - 1
+      end
+    end
+    result
+  end
+end
